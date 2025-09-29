0063609d82ea8bf3267cc54dacda639239793669
cnathanraecentgattud-spec <cnathanraecentgattud@gmail.com>
2025-09-29 11:39:36 +0800

    introintro

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ def intro():
     print("Welcome to (insert game name)!")
     name = input("What is your name? ")
     time.sleep(2)
-    print(f"{name.upper()}?!")
+    print(f"{name}..")
     print("...")
     time.sleep(2)
+    print("born april 11, 1954
     print(f"oh how i've long foreseen your arrival here, {name}..")
     print("now, where would we like to begin?")
+    
 
 intro()
 
