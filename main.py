364232f73e89bc8af12a8f6baa2f9102fffd627b
cnathanraecentgattud-spec <cnathanraecentgattud@gmail.com>
2025-10-01 11:18:09 +0800

    full intro
    pls connect to orig laptop

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import time
 def intro():
+    choice = 0
     print("Welcome to (insert game name)!")
     name = input("What is your name? ")
     time.sleep(2)
     print(f"{name}..")
     print("...")
     time.sleep(2)
-    print("born april 11, 1954
-    print(f"oh how i've long foreseen your arrival here, {name}..")
+    print("born april 11, 1954..")
+    print("designated for the earliest bus en route to purgatory..")
+    time.sleep(1)
+    print("based on your credentials, you are eligible for a free reading on your mortal life.")
+    time.sleep(1)
     print("now, where would we like to begin?")
-    
-
+    time.sleep(1)
+    print()
+    print("ENTER 1 TO BE ENLIGHTENED")
+    time.sleep(1)
+    print("ENTER 2 TO REMINISCE")
+    time.sleep(1)
+    print("ENTER 3 TO BOARD")
+    input(choice)
+    if choice == 1:
+        def test
+            elif choice == 2:
+                def test
+                    elif choice == 3:
+                        def test
+                        
 intro()
 
 
